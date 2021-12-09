a6a87aef31c0a12648461b8f1ef876aff7aa27ec
Marina Hrytsai <polanic@ukr.net>
2021-12-09 22:53:38 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector("#controls input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+let size = 30;
+
+const createBoxes = (amount) => {
+  let string = "";
+
+  // if (amount === "" || amount < 0 || amount > 100) {
+  //   console.log("ты редиска", amount);
+  //   return;
+  // }
+
+  for (let i = 0; i < amount; i += 1) {
+    string += `<div style="background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px"></div>`;
+    size += 10;
+  }
+
+  refs.boxes.insertAdjacentHTML("beforeend", string);
+};
+
+const destroyBoxes = () => {
+  refs.boxes.innerHTML = "";
+  size = 30;
+};
+
+const fn = () => createBoxes(parseInt(refs.input.value));
+
+refs.btnCreate.addEventListener("click", fn);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
